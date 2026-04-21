b4b501af27b8e8eb66047f2d6e89b579147aea8d
ElleBeders <malefisenta135@gmail.com>
2026-04-21 09:20:44 +0500

    Add files via upload

---
+++ C22/C2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Создание треугольника 1: ");
+        RightTriangle tri1 = Create();
+        Print(tri1, 1);
+        Console.WriteLine();
+        Console.WriteLine("Создание треугольника 2: ");
+        RightTriangle tri2 = Create();
+        Print(tri2, 2);
+        Console.WriteLine();
+
+        Console.WriteLine("Уменьшаем треугольник 1: ");
+        Print(--tri1, 1);
+        Console.WriteLine();
+        Console.WriteLine("Увеличиваем треугольник 2: ");
+        Print(++tri2, 2);
+        Console.WriteLine();
+
+        double s_tr1 = (double)tri1;
+        double s_tr2 = (double)tri2;
+        Console.WriteLine("Площадь треугольника 1: " + s_tr1);
+        Console.WriteLine("Площадь треугольника 2: " + s_tr2);
+        Console.WriteLine();
+
+        Srav(tri1, tri2);
+
+    }
+
+    public static void Srav(RightTriangle tr1, RightTriangle tr2)
+    {
+        Console.WriteLine("Сравнение треугольников:");
+        bool b_tr_1 = (tr1 <= tr2);
+        bool b_tr_2 = (tr1 >= tr2);
+        if ((b_tr_1 == b_tr_2)&&((double)tr1 == (double)tr2))
+        {
+            Console.WriteLine("Треугольники равны.");
+        }
+        else
+        {
+            Console.WriteLine("Треугольники не равны.");
+            if (b_tr_1)
+            {
+                Console.WriteLine("Треугольник 2 больше.");
+            }
+            else 
+            {
+                Console.WriteLine("Треугольник 1 больше.");
+            }
+        }
+    }
+
+    public static void Print(RightTriangle tr, int number)
+    {
+        Console.WriteLine("Треугольник " + number + " : " + tr.ToString());
+    }
+
+    public static RightTriangle Create()
+    {
+        Console.Write("Катет 1 = ");
+        double a = Check();
+        Console.Write("Катет 2 = ");
+        double b = Check();
+        RightTriangle tri = new RightTriangle(a, b);
+        if (tri)
+        {
+            Console.WriteLine("Треугольник существует.");
+        }
+        else
+        {
+            Console.WriteLine("Треугольник не существует.");
+        }
+        return tri;
+    }
+
+    public static double Check()
+    {
+        double a = 1;
+        int i = 0;
+        int k = 0;
+        while (i == 0)
+        {
+            if (k != 0)
+            {
+                Console.Write("Неверный ввод, повторите попытку: ");
+            }
+            string input = Console.ReadLine();
+            if (double.TryParse(input, out a) == true)
+            {
+                i = 1;
+            }
+            k++;
+            if (k == 5)
+            {
+                a = 1;
+                i = 1;
+            }
+        }
+        return a;
+    }
+}
---
+++ C22/C2/RightTriangle .cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+internal class RightTriangle
+{
+    private double _a; 
+    private double _b; 
+
+    public RightTriangle(double sideA, double sideB)
+    {
+        A = sideA;
+        B = sideB;
+    }
+
+    public RightTriangle(RightTriangle other)
+    {
+        _a = other._a;
+        _b = other._b;
+    }
+
+    public double A
+    {
+        get 
+        { 
+            return _a; 
+        }
+        set
+        {
+            if (value > 0)
+            {
+                _a = value;
+            }
+            else
+            {
+                _a = 1;
+            }
+        }
+    }
+
+    public double B
+    {
+        get 
+        { 
+            return _b; 
+        }
+        set
+        {
+            if (value > 0)
+            {
+                _b = value;
+            }
+            else
+            {
+                _b = 1;
+            }
+        }
+    }
+
+    public double GetArea()
+    {
+        return (_a * _b) / 2;
+    }
+
+    public static implicit operator bool(RightTriangle Tr)
+    {
+        return Tr._a > 0 && Tr._b > 0;
+    }
+
+    public static explicit operator double(RightTriangle Tr)
+    {
+        if (Tr)
+        {
+            return Tr.GetArea();
+        }
+        else 
+        {
+            return -1;
+        }
+    }
+
+    public static RightTriangle operator ++(RightTriangle Tr)
+    {
+        return new RightTriangle(Tr._a * 2, Tr._b * 2);
+    }
+
+    public static RightTriangle operator --(RightTriangle Tr)
+    {
+        return new RightTriangle(Tr._a / 2, Tr._b / 2);
+    }
+
+    public static bool operator <=(RightTriangle Tr1, RightTriangle Tr2)
+    {
+        return Tr1.GetArea() <= Tr2.GetArea();
+    }
+
+    public static bool operator >=(RightTriangle Tr1, RightTriangle Tr2)
+    {
+        return Tr1.GetArea() >= Tr2.GetArea();
+    }
+
+    public override string ToString()
+    {
+        return "(" + _a + ", " + _b + ")";
+    }
+}
